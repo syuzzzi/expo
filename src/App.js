98b7d1376ef88f9483b7944e42ab89b9e9f72c15
suji <csj1430@naver.com>
2025-01-19 23:57:23 +0900

    flexbox까지

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
-import React, { useState } from 'react';
-import { StyleSheet, View, Text, TextInput } from 'react-native';
-import MyButton from './MyButton';
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+import Box from './Box';
 
 export default function App() {
-  const [addition, setAddition] = useState(0);
-  const [multiple, setMultiple] = useState(1);
-
   return (
     <View style={styles.container}>
-      <TextInput 
-      //onChange={event => console.log(event.nativeEvent.text)}
-      onChangeText={text => console.log(text)}
-      style={{borderWidth: 1, padding: 10, fontSize: 20}} />
-      {/*<Text style={{fontSize: 20}}>{addition}</Text>
-      <Text style={{fontSize: 20}}>{multiple}</Text>
-      <MyButton title='addition' onPress={() => setAddition(addition + 2)}/>
-      <MyButton title='multiple' onPress={() => setMultiple(multiple * 2)}/>*/}
+      <StatusBar style="auto" />
+      <Box style={{ backgroundColor: 'red', flex: 1}} />
+      <Box style={{ backgroundColor: 'green', flex: 1}} />
+      <Box style={{ backgroundColor: 'blue', flex: 1}} />
     </View>
   );
 }
