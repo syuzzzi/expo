5823ff8c0d8521cb9f5a4fdd818a05db1577ef98
suji <csj1430@naver.com>
2025-01-20 23:14:42 +0900

    Styled-components까지

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
-import Shadow from './Shadow';
+import styled, { css } from 'styled-components/native';
+
+const Container = styled.View`
+   flex: 1;
+    background-color:#8a8a8a;
+    align-items: center;
+    justify-content: center;
+`;
+
+const cssText = css`
+  font-size: 20px;
+  font-weight: 600;
+`;
+const StyledText = styled.Text`
+  ${cssText}
+  color: blue;
+`;
+const ErrorText = styled(StyledText)`
+  ${cssText}
+  color: red;
+`;
+const StyledButton = styled.Button``;
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <Container>
       <StatusBar style="auto" />
-      <Shadow />
-    </View>
+      <StyledText>styled component</StyledText>
+      <ErrorText>Error !!</ErrorText>
+      <StyledButton title="Styled" onPress={() => alert('styled !!')}></StyledButton>
+    </Container>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
